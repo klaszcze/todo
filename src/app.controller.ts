3eb175a82eccb2af75c29f9ca06aa243eb9d09fc
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2020-01-29 15:42:34 +0100

    Fix patch

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,51 +1,68 @@
-import { Controller, Get, Param, Post, Body, UsePipes, ValidationPipe, Delete, HttpCode, HttpStatus, Patch } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ICreateTodo } from './interfaces/ICreateTodo'
-import { ITodo } from './interfaces/ITodo'
-import { thistle } from 'color-name';
-import { Todo } from './todo'
-import { TodoValidator, TodoUpdateValidator } from './todo-validator';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Patch
+} from "@nestjs/common";
+import { AppService } from "./app.service";
+import { ICreateTodo } from "./interfaces/ICreateTodo";
+import { ITodo } from "./interfaces/ITodo";
+import { thistle } from "color-name";
+import { Todo } from "./todo";
+import { TodoValidator, TodoUpdateValidator } from "./todo-validator";
 
 @UsePipes(new ValidationPipe())
-
-@Controller('/todos')
+@Controller("/todos")
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  private database:{ [id:number] : ITodo} = {};
+  private database: { [id: number]: ITodo } = {};
 
   @Get()
   getTodos(): ITodo[] {
-    return Object.values(this.database)
+    return Object.values(this.database);
   }
 
-  @Get('/:id')
-  getTodo(@Param('id') id: string): ITodo {
-    return this.database[id]
+  @Get("/:id")
+  getTodo(@Param("id") id: string): ITodo {
+    return this.database[id];
   }
 
   @Post()
   createTodo(@Body() createData: TodoValidator): ITodo {
-    const todo = new Todo(createData.title, createData.order)
-    this.database[todo.id] = todo
+    const todo = new Todo(createData.title, createData.order);
+    this.database[todo.id] = todo;
     return todo;
   }
 
   @Delete()
   @HttpCode(HttpStatus.NO_CONTENT)
   deleteAll(): void {
-    this.database = {}
+    this.database = {};
   }
 
-  @Delete(':id')
+  @Delete(":id")
   @HttpCode(HttpStatus.NO_CONTENT)
-  deleteOne(@Param('id') id: string): void {
-    delete this.database[id]
-  }
-  
-  @Patch(':id')
-  updateTodo(@Param('id') id: string, @Body() updateData: TodoUpdateValidator): ITodo {
-    this.database[id] = updateData
-    return this.database[id]
+  deleteOne(@Param("id") id: string): void {
+    delete this.database[id];
   }
 
+  @Patch(":id")
+  updateTodo(
+    @Param("id") id: string,
+    @Body() updateData: TodoUpdateValidator
+  ): ITodo {
+    const updatedTodo = {
+      ...this.database[id],
+      ...updateData
+    };
+    this.database[id] = updatedTodo;
+    return this.database[id];
+  }
 }
